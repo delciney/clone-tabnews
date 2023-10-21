b9c25fe5b6fca8a0e98ba8eb971aad8fb58646a0
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2023-10-21 16:28:57 +0000

    Criação da estrutura inicial do projeto

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return <div>
+            <h1>
+                E ao primo eu estou fazendo o curso do Filipe Deschamps
+            </h1>
+            <h3>Assim que eu terminar é a sua vez de passar por essa jornada!</h3>
+           </div>
+}
+
+export default Home;
