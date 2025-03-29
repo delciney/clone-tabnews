79127accbf27da5ff32bd027bdede08d7aba9014
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-03-29 17:29:28 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br desenvolvido com as aulas do https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-function Home () {
-    return <div>
-            <h1>
-                E ao primo eu estou fazendo o curso do Filipe Deschamps
-            </h1>
-            <h3>Assim que eu terminar é a sua vez de passar por essa jornada!</h3>
-           </div>
+function Home() {
+  return (
+    <div>
+      <h1>E ao primo eu estou fazendo o curso do Filipe Deschamps</h1>
+      <h3>Assim que eu terminar é a sua vez de passar por essa jornada!</h3>
+    </div>
+  );
 }
 
 export default Home;
