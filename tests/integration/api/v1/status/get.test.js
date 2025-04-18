3434df6c1c7f53267f0ac31b588f174d451924d2
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-04-18 19:49:48 +0000

    Adicionado teste do endpoint ``/api/v1/status``

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,13 @@
+test("Get to /api/v1/status should return status 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const data = await response.json();
+  expect(response.status).toBe(200);
+});
+
+test("Get to /api/v1/status should return ok", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const data = await response.json();
+  expect(data).toEqual({
+    status: "Ok",
+  });
+});
