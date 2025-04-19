1b9cf7e524e994baa2bf200e7d7cf76f519c5019
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-04-19 02:39:45 +0000

    Removida variável não utilizada

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,5 @@
 test("Get to /api/v1/status should return status 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  const data = await response.json();
   expect(response.status).toBe(200);
 });
 
