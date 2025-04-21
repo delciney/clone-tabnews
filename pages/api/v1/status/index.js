87744830daecc5a0ebcea3f21ff24ce49244f41e
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-04-21 20:51:39 +0000

    Finalizado endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "Ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+  const databaseOpenConnectionsValue =
+    databaseOpenConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        open_connections: databaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
