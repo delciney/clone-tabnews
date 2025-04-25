88c23ed3242244da2ee1feb773c7eb93262315ad
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-04-25 21:41:35 +0000

    Refatoração do fluxo migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,27 +2,30 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(req, res) {
-  if (req.method !== "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    res.status(200).json(migrations);
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (req.method == "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    return res.status(200).json(pendingMigrations);
   }
   if (req.method == "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    res.status(200).json(migrations);
+
+    if (migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations);
+    }
+
+    return res.status(200).json(migratedMigrations);
   }
 
   return res.status(405).end();
