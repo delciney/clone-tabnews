a046ebc7b2163bbc56f204e9d40172fc47b97d24
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-04-30 00:39:50 +0000

    Adicionado script `wait-for-postgres`

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write("🟨");
+      checkPostgres();
+      return;
+    }
+    console.log("\n\n✅ PostgreSQL is ready\n");
+  }
+}
+
+process.stdout.write("\n\n🛑 Waiting for PostgreSQL to be ready...\n");
+
+checkPostgres();
