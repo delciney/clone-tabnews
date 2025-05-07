c3aaa66bec2657783e622ed944605e5c9533fcbc
DELCINEY LEMOS OLIVEIRA <delciney07@gmail.com>
2025-05-07 00:48:38 +0000

    test: rename all description to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,12 +8,16 @@ beforeAll(async () => {
   );
 });
 
-test("Get to /api/v1/migrations should return status 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
